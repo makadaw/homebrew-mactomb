d352b7647f2930f95c2fb1e20814fe1a31d19518
Alex <bendernumber.14@gmail.com>
2017-09-28 22:41:51 +0200

    Update mactomb formula

--- Formula/mactomb.rb
+++ Formula/mactomb.rb
@@ -1,20 +1,15 @@
-class Mactomb < Formula
-  desc "MacTomb is a free and open source system for file encryption on Mac OS X, facilitating the backup of secret files through encrypted storage containers."
-  homepage "https://www.dyne.org/software/mactomb/"
-  url "https://github.com/makadaw/MacTomb/archive/master.zip"
-  sha256 "c2c008c44ff567e6af8df4cc5a74957060ce40de0d423d1d41d6f8ddecaa4428"
-  version "1.3.0"
+require "fileutils"
 
-  # depends_on "cmake" => :build
+class Mactomb < Formula
+  desc "Is a free and open source system for file encryption on Mac OS X, facilitating the backup of secret files through encrypted storage containers."
+  homepage "https://www.dyne.org/software/mactomb/"
+  url "https://github.com/makadaw/MacTomb/archive/1.3.0.zip"
+  sha256 "c2c008c44ff567e6af8df4cc5a74957060ce40de0d423d1d41d6f8ddecaa4428"
 
   def install
-    # ENV.deparallelize
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    # Set exec rights to script
+    cp("mactomb.sh", "mactomb")
+    bin.install "mactomb"
   end
 
   test do
