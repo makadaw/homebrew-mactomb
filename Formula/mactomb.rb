335c69bd8dc7b1adae183eefa9bc956dceaf910c
Alex <makadaw@gmail.com>
2017-10-02 10:17:01 +0200

    Change sha for current version of mactomb

--- Formula/mactomb.rb
+++ Formula/mactomb.rb
@@ -4,7 +4,7 @@ class Mactomb < Formula
   desc "Is a free and open source system for file encryption on Mac OS X, facilitating the backup of secret files through encrypted storage containers."
   homepage "https://www.dyne.org/software/mactomb/"
   url "https://github.com/makadaw/MacTomb/archive/1.3.0.zip"
-  sha256 "c2c008c44ff567e6af8df4cc5a74957060ce40de0d423d1d41d6f8ddecaa4428"
+  sha256 "8703932f72bbc0dbebc0a7f88a6aff3ccd650b5583349dcab0f0c6568e3cfb18"
 
   def install
     # Set exec rights to script
