3eb7bbe72aa4f6d5607a2cfb89ca43e05a0381e2
Mihhail Sidorin <msidorin@gmail.com>
2020-10-22 11:42:29 +0300

    Version 1.4 update for mactomb formula
    https://github.com/davinerd/MacTomb#whats-new-v14

--- Formula/mactomb.rb
+++ Formula/mactomb.rb
@@ -3,7 +3,7 @@ require "fileutils"
 class Mactomb < Formula
   desc "Is a free and open source system for file encryption on Mac OS X, facilitating the backup of secret files through encrypted storage containers."
   homepage "https://www.dyne.org/software/mactomb/"
-  url "https://github.com/makadaw/MacTomb/archive/1.3.0.zip"
+  url "https://github.com/makadaw/MacTomb/archive/1.4.0.zip"
   sha256 "8703932f72bbc0dbebc0a7f88a6aff3ccd650b5583349dcab0f0c6568e3cfb18"
 
   def install
